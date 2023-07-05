4941b023fcad93a49a9b0aed35953da46dcbbf78
rencheeraj.mohan <>
2023-07-05 13:40:09 +0530

    FirstCoordinator extension Drawable

--- Coordinator Design Pattern/Coordinator Design Pattern/FirstView/FirstCoordinator.swift
+++ Coordinator Design Pattern/Coordinator Design Pattern/FirstView/FirstCoordinator.swift
@@ -12,24 +12,34 @@ class FirstCoordinator : BaseCoordinator{
     init(router: RouterProtocol) {
         self.router = router
     }
+    lazy var myViewController: ViewController = {
+        let viewModel = FirstViewModel()
+        let viewController = ViewController(viewModel: viewModel)
+        return viewController
+        }()
     override func start() {
 
         // prepare the associated view and injecting its viewModel
-        let viewModel = FirstViewModel()
-        let viewController = ViewController(viewModel: viewModel)
+//        let viewModel = FirstViewModel()
+//        let myViewController = ViewController(viewModel: viewModel)
 
         // for specific events from viewModel, define next navigation
-        viewModel.didSelect = { [weak self] in
+        myViewController.viewModel.didSelect = { [weak self] in
             guard let strongSelf = self else { return }
             strongSelf.showNextScreen(in: strongSelf.router)
         }
 
-        router.push(viewController, isAnimated: true, onNavigateback: self.isCompleted)
+        router.push(myViewController, isAnimated: true, onNavigateback: self.isCompleted)
     }
 
     // we can go further in our flow if we need to
     func showNextScreen(in router: RouterProtocol) {
         let newCoordinator = FirstCoordinator(router: router)
         self.start(coordinator: newCoordinator)
+    }
+}
+extension FirstCoordinator : Drawable {
+    var viewController: UIViewController {
+        return myViewController
     }
 }
